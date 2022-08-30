53cfe79c02748cf150f7eeccbf3168e2d4b77be8
VladyslavGilyuk <vladgilyuk@gmail.com>
2022-08-30 17:17:05 +0300

    Sunset, sunrise added. Time converted to new Date.

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 const cityLabel = document.querySelector(".city")
 const descriptionLabel = document.querySelector(".description")
+const iconLabel = document.querySelector(".icon")
 const temperatureLabel = document.querySelector(".temperature");
 const humidityLabel = document.querySelector(".humidity")
 const windLabel = document.querySelector(".wind")
+const sunriseLabel = document.querySelector(".sunrise")
+const sunsetLabel = document.querySelector(".sunset")
 const searchButton = document.querySelector(".search button")
 const searchBar = document.querySelector(".search-bar")
 
@@ -24,14 +27,17 @@ let weather = {
     displayWeather: function(data) {
         const {name} = data 
         const {country, sunrise, sunset} = data.sys
-        const {description} = data.weather[0];
+        const {description, icon} = data.weather[0];
         const {temp, humidity} = data.main 
         const {speed} = data.wind
-        cityLabel.innerText = `Weather in ${name}`;
+        cityLabel.innerText = `Weather in ${name}, ${country}`;
+        iconLabel.src = `https://openweathermap.org/img/wn/${icon}.png`;
         descriptionLabel.innerText = `${description}`;
-        temperatureLabel.innerText = `${temp} °C`;
+        temperatureLabel.innerText = `${Math.floor(temp)} °C`;
         humidityLabel.innerText = `Humidity: ${humidity} %`;
         windLabel.innerText = `Wind: ${speed} km/h`;
+        sunriseLabel.innerText = `Sunrise: ${new Date(1000* sunrise).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}`;
+        sunsetLabel.innerText = `Sunset: ${new Date(1000* sunset).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}`;
         
         document.body.style.backgroundImage =
         `url("https://source.unsplash.com/1600x900/?${name})"`;
